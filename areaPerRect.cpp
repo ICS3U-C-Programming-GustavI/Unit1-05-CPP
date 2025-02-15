993b9180b029e4d21ac97c14fc0e02d9216d8808
Gustav Ihlenfeld <gustav.ihlenfeld@stu.ocsb.ca>
2025-02-15 16:29:19 +0000

    Added areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2025 Gustav I All rights reserved.
+// Created by: Gustav I
+// Created on: Feb 15, 2025
+// This program calculates and displays the area and
+// perimeter of a rectangle with dimensions 6cm X 8cm.
+#include <iostream>
+
+int main() {
+    std::cout << "For a rectangle that has dimensions\n";
+    std::cout << "length 6cm and width 8cm:\n";
+    std::cout << "\n";
+    std::cout << "Area = " << 6 * 8 << " cm^2\n";
+    std::cout << "Perimeter = " << 2 * (6 + 8) << " cm\n";
+}
